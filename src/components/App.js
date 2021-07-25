177e26344dae663a79612b040d14e14a5b66f26d
Kelvin Lin <69781419+kelvinlin97@users.noreply.github.com>
2021-07-25 06:18:20 -0400

    Style sidebar (icons, etc.) (#5)

--- src/components/App.js
+++ src/components/App.js
@@ -8,28 +8,27 @@ import Analytics from './Analytics'
 function App() {
   return (
     <Router>
-        <nav id="sideNavbar">
-          <img id="landis-logo" src="https://i.imgur.com/2EWStos.png"/>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/accounts">Accounts</Link>
-            </li>
-            <li>
-              <Link to="/analytics">Analytics</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/accounts" component={Accounts}>
-          </Route>
-          <Route path="/analytics" component={Analytics}>
-          </Route>
-          <Route path="/" component={Accounts}>
-          </Route>
-        </Switch>
+      <nav id="sideNavbar">
+        <img id="landis-logo" src="https://i.imgur.com/2EWStos.png" />
+        <ul id="sidebar-links">
+          <li className="icon-container">
+          <img className="icon" src="https://www.iconpacks.net/icons/1/free-user-icon-244-thumb.png" />
+            <Link className="links" to="/accounts">Accounts</Link>
+          </li>
+          <li id="space-analytics-link">
+          <img className="icon" src="https://static.thenounproject.com/png/16201-200.png" />
+            <Link className="links" to="/analytics">Analytics</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/accounts" component={Accounts}>
+        </Route>
+        <Route path="/analytics" component={Analytics}>
+        </Route>
+        <Route path="/" component={Accounts}>
+        </Route>
+      </Switch>
     </Router>
   );
 }
