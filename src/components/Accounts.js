5fdb94d7fc1d0bb51cbad7e2fb326bc891b3b2e7
Kelvin Lin <69781419+kelvinlin97@users.noreply.github.com>
2021-07-25 02:09:43 -0400

    Create accounts components (cards, tooltips, mortgage indicator) (#3)
    * Create Cards on Account Component, Add progress bar for each account, refactor styling, and add tooltip

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,16 +1,41 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react"
 import { connect } from "react-redux";
 import { fetchAccounts } from "../redux/accounts"
+import AccountCard from './Cards'
+import { Container, Row, Col } from "react-bootstrap";
 
 class Accounts extends React.Component {
+  constructor(props){
+    super(props);
+    this.indicateMortgage = this.indicateMortgage.bind(this);
+  }
+  indicateMortgage(score, balance) {
+    //Mortgage likelihood is scaled from 0 - 100 with equal weight given to balance and credit score
+    const typeConvertedBal = Number(balance);
+    const scaledCreditScore = score / 850;
+    const weightedCredit = scaledCreditScore * 50;
+    const scaledBalance = typeConvertedBal / 20000
+    const weightedBalance = scaledBalance * 50;
+    return Math.floor(weightedCredit + weightedBalance)
+  }
   componentDidMount() {
     this.props.getAccounts();
   }
-  render(){
+  render() {
     let accounts = this.props.accounts;
-    console.log(accounts)
     return (
-      'no'
+      <Container>
+      <Row xs={1} md={2} className="g-4">
+        {accounts.map((account, index) => {
+          return (
+            <Col class="col-md-3">
+            <AccountCard account={account} indicateMortgage={this.indicateMortgage} key={index}/>
+            </Col>
+          )
+        })}
+      </Row>
+      </Container>
     )
   }
 }
